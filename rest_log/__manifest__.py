a98c17747f974597d1880b3a85627c2bf23e4693
Mohamed Helmy <helmy419@gmail.com>
2024-05-12 14:45:13 +0300

    update to 17

--- rest_log/__manifest__.py
+++ rest_log/__manifest__.py
@@ -1,11 +1,7 @@
-# Copyright 2020 Camptocamp SA (http://www.camptocamp.com)
-# Copyright 2021 ACSONE SA/NV (http://www.acsone.eu)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
 {
     "name": "REST Log",
     "summary": "Track REST API calls into DB",
-    "version": "15.0.1.0.0",
+    "version": "17.0.0.0.0",
     "development_status": "Beta",
     "website": "https://github.com/OCA/rest-framework",
     "author": "Camptocamp, ACSONE, Odoo Community Association (OCA)",
@@ -20,5 +16,5 @@
         "views/rest_log_views.xml",
         "views/menu.xml",
     ],
-    "installable": False,
+    "installable": True,
 }
