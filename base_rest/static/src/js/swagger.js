5c7ef17c665956fce8ad429ef250589cbff9d21e
Mohamed Helmy <helmy419@gmail.com>
2024-05-12 14:41:22 +0300

    update base restapi to 17

--- base_rest/static/src/js/swagger.js
+++ base_rest/static/src/js/swagger.js
@@ -1,16 +1,17 @@
-odoo.define("base_rest.swagger", function (require) {
-    "use strict";
+/** @odoo-module **/
 
-    var publicWidget = require("web.public.widget");
-    var SwaggerUi = require("base_rest.swagger_ui");
+import publicWidget from '@web/legacy/js/public/public_widget';
+import SwaggerUi from '@base_rest/js/swagger_ui';
 
-    publicWidget.registry.Swagger = publicWidget.Widget.extend({
-        selector: "#swagger-ui",
-        start: function () {
-            var def = this._super.apply(this, arguments);
-            var swagger_ui = new SwaggerUi("#swagger-ui");
-            swagger_ui.start();
-            return def;
-        },
-    });
+publicWidget.registry.Swagger = publicWidget.Widget.extend({
+  selector: "#swagger-ui",
+  start: function () {
+      var def = this._super.apply(this, arguments);      
+      var swagger_ui = new SwaggerUi("#swagger-ui");
+      swagger_ui.start();
+      return def;
+  },
 });
+
+
+
