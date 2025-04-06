2539c3d5e72cfe9fa02d3a823c57a9121efadfff
Mohamed Helmy <helmy419@gmail.com>
2025-04-06 14:27:22 +0200

    Update __manifest__.py

--- rest_log/__manifest__.py
+++ rest_log/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "REST Log",
     "summary": "Track REST API calls into DB",
-    "version": "17.0.0.0.0",
+    "version": "18.0.0.0.0",
     "development_status": "Beta",
     "website": "https://github.com/OCA/rest-framework",
     "author": "Camptocamp, ACSONE, Odoo Community Association (OCA)",
