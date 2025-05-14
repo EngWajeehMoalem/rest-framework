9e7a28f5c2a39368cd5dca7f2a8ea31515fed4a3
Mohamed Helmy <helmy419@gmail.com>
2025-05-14 17:13:31 +0300

    update rest log

--- base_rest/__manifest__.py
+++ base_rest/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": """
         Develop your own high level REST APIs for Odoo thanks to this addon.
         """,
-    "version": "17.0.0.0.1",
+    "version": "18.0.0.0.1",
     "development_status": "Beta",
     "license": "LGPL-3",
     "author": "ACSONE SA/NV, " "Odoo Community Association (OCA)",
