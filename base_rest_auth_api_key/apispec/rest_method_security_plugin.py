6b398c8cda7394bbc4df67df8dd61945f212919a
Mohamed Helmy <helmy419@gmail.com>
2025-05-14 17:24:51 +0300

    update rest log

--- base_rest_auth_api_key/apispec/rest_method_security_plugin.py
+++ base_rest_auth_api_key/apispec/rest_method_security_plugin.py
@@ -18,7 +18,7 @@ class RestMethodSecurityPlugin(BasePlugin):
         spec.components.security_scheme("api_key", api_key_scheme)
 
     def operation_helper(self, path=None, operations=None, **kwargs):
-        routing = kwargs.get("routing")
+        routing = kwargs.get("original_routing")
         if not routing:
             super(RestMethodSecurityPlugin, self).operation_helper(
                 path, operations, **kwargs
